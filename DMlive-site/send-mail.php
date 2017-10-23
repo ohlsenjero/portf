484ac64f21e4642a5fc4f0b8369d62547836aa0b
ohlsenjero <jerohlsen@gmail.com>
2017-10-23 13:54:44 +1300

    Add files via upload

---
+++ DMlive-site/send-mail.php
@@ -0,0 +1,17 @@
+<?php
+define('RECIPIENT','jerohlsen@gmail.com');
+//catch the posted data
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = "Enquiry from Website";
+$message = $_POST['message'];
+//set the email body and headers
+$body = $message."\n\n";
+$body.= $name."<$email>";
+$headers = "From: $name<$email>";
+if(mail(RECIPIENT, $subject, $body, $headers)):
+echo "success";
+else:
+echo "error";
+endif;
+?>
